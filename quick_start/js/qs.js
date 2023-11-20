66f3a6243d601b2a73d2e4d6709345505529e373
Fernanda Caetano <fernandacunha@id.uff.br>
2023-11-20 11:12:50 -0300

    dealing with events

--- quick_start/js/qs.js
+++ quick_start/js/qs.js
@@ -2,7 +2,8 @@ var map = L.map('map').setView([-22.906, -43.190], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright"\
+				 >OpenStreetMap</a>'
 }).addTo(map);
 
 var home = L.marker([-22.937, -43.177]).addTo(map);
@@ -24,3 +25,21 @@ var campus42 = L.polygon([
 home.bindPopup("Home").openPopup();
 niltao.bindPopup("Second home.");
 campus42.bindPopup("42 Rio campus");
+
+var popup = L.popup()
+    .setLatLng([-22.911108, -43.197522])
+    .setContent("I am a standalone popup.")
+    // .openOn(map); //Handles closing of previously opened popups
+    .addTo(map); //Doesn't handle closing of previously opened popups,
+					//home popup will remain open
+
+var coord = L.popup();
+
+function onMapClick(e) {
+	coord
+		.setLatLng(e.latlng)
+		.setContent("Coordinate: " + e.latlng.toString())
+		.openOn(map);
+}
+
+map.on('click', onMapClick);
