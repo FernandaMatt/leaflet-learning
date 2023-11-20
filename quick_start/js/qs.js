6bd5fea0825d19c3ba9b7a55a14e0e1b3d6c9739
Fernanda Caetano <fernandacunha@id.uff.br>
2023-11-20 10:30:12 -0300

    adding homes

--- quick_start/js/qs.js
+++ quick_start/js/qs.js
@@ -1,6 +1,15 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([-22.906, -43.190], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var home = L.marker([-22.937, -43.177]).addTo(map);
+
+var niltao = L.circle([-22.8933, -43.2925], {
+	color: 'black',
+	fillColor: '#fff',
+	fillOpacity: 0.7,
+	radius: 250
 }).addTo(map);
